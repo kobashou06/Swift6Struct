4e66e1c4cb4adb4372261d01757af2fa86b3d6aa
kobashou06 <kobashou06@gmail.com>
2021-01-08 14:01:59 +0900

    Initial Commit

---
+++ Swift6Struct/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift6Struct
+//
+//  Created by kobashou06 on 2021/01/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
